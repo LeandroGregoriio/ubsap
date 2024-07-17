5a3139bef1e037a9808c1984d45d41d5abb7f04b
Leandro Gregorio <lg.leo.2014@gmail.com>
2024-07-16 21:17:22 -0300

    Última versão

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { collection, getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC3NdW4zyyHWLX_sHXfVJ-_1llco0HcmXc",
   authDomain: "tcc---iubs.firebaseapp.com",
@@ -14,7 +12,12 @@ const firebaseConfig = {
   appId: "1:82300384414:web:2f1ce6124e1a9a19d7f78f"
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const database = getFirestore(app);
+// Certifique-se de que essa inicialização acontece apenas uma vez
+const app = initializeApp(firebaseConfig);
+
+// Certifique-se de que essas chamadas são feitas onde necessário
+const database = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { app, database, auth, storage };
